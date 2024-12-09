881e2fa8362a21a03c404bac8fecfea51f62cc20
drowlia <lsx3193161830@163.com>
2024-12-09 17:54:18 +0800

    初步完成了顺序表,开始debug

--- SeqTable/SeqTable.hpp
+++ SeqTable/SeqTable.hpp
@@ -19,7 +19,7 @@ struct SeqTable {
 	explicit SeqTable(size_t size) :
 		size_{size},
 		capacity_{2 * size},
-		array_{size ? new ElemType[2 * size] : nullptr} {
+		array_{size ? new ElemType[capacity_] : nullptr} {
 			if (size == 0)
 				throw std::invalid_argument("size of SeqTable can't be zero");
 	}
@@ -31,19 +31,143 @@ struct SeqTable {
 	SeqTable(const SeqTable& ano_table) :
 		size_{ano_table.size_},
 		capacity_{ano_table.capacity_},
-		array_{ano_table.size_ ? new ElemType[2 * ano_table.size_] : nullptr} {
-			if (ano_table.size_ == 0)
-				throw std::invalid_argument("size of SeqTable can't be zero");
-
-			std::copy(ano_table.array_, ano_table.array_ + size_)
+		array_{ano_table.size_ ? new ElemType[ano_table.capacity_] : nullptr} {
+			if (ano_table.size_ > 0) {
+				std::copy(ano_table.array_, ano_table.array_ + size_, array_);
+			}
 	}
+
+	SeqTable& operator=(const SeqTable& ano_table) {
+		if (this == &ano_table)
+			return *this;
+
+		delete[] array_;
+
+		size_ = ano_table.size_;
+		capacity_ = ano_table.capacity_;
+		array_ = ano_table.size_ ? new ElemType[ano_table.capacity_] : nullptr;
+
+		if (ano_table.size_ > 0) {
+			std::copy(ano_table.array_, ano_table.array_ + size_, array_);
+		}
+
+		return *this;
+	}
+
+	SeqTable(SeqTable&& ano_table) noexcept :
+		size_{ano_table.size_},
+		capacity_{ano_table.capacity_},
+		array_{ano_table.array_} {
+			ano_table.size_ = 0;
+			ano_table.capacity_ = 0;
+			ano_table.array_ = nullptr;
+	}
+
+	SeqTable& operator=(SeqTable&& ano_table) noexcept {
+		if (this == &ano_table)
+			return *this;
+
+		delete[] array_;
+
+		size_ = ano_table.size_;
+		capacity_ = ano_table.capacity_;
+		array_ = ano_table.array_;
+
+		ano_table.size_ = 0;
+		ano_table.capacity_ = 0;
+		ano_table.array_ = nullptr;
+
+		return *this;
+	}
+
+	void resize(const size_t& new_size) {
+		if (new_size == 0) {
+			delete[] array_;
+			array_ = nullptr;
+			size_ = 0;
+			capacity_ = 0;
+			return;
+		}
+
+		auto *new_array = new ElemType[new_size]();
+		auto copy_size = std::min(size_, new_size);
+		std::copy(array_, array_ + copy_size, new_array);
+
+		delete[] array_;
+		size_ = new_size;
+		capacity_ = new_size * 2;
+		array_ = new_array;
+	}
+
+	void push_back(const ElemType& elem) {
+		if (size_ >= capacity_)
+			resize(capacity_ == 0 ? 1 : capacity_ * 2);
+
+		array_[size_] = elem;
+		++size_;
+	}
+
+	void insert(const size_t& pos, const ElemType& elem) {
+		if (pos > size_) {
+			throw std::out_of_range("Index out of range");
+		}
+
+		if (size_ >= capacity_) {
+			resize(capacity_ == 0 ? 1 : capacity_ * 2);
+		}
+
+		std::move(array_ + pos, array_ + size_, array_ + pos + 1);
+
+		array_[pos] = elem;
+		++size_;
+	}
+
+	void pop_back() {
+		if (size_ > 0)
+			--size;
+	}
+
+	void erase(const size_t& pos) {
+		if (pos >= size_) {
+			throw std::out_of_range("Index out of range");
+		}
+
+		std::move(array_ + pos + 1, array_ + size_, array_ + pos);
+
+		--size_;
+	}
+
+
+	[[nodiscard]] const ElemType& at (const size_t& pos) const {
+		if (pos >= size_)
+			throw std::out_of_range("Index out of range");
+
+		return array_[pos];
+	}
+
+	[[nodiscard]] ElemType& operator[](const size_t& pos) {
+		if (pos >= size_)
+			throw std::out_of_range("Index out of range");
+
+		return array_[pos];
+	}
+
+	[[nodiscard]] ElemType& front() {
+		return array_[0];
+	}
+
+	[[nodiscard]] ElemType& back() {
+		return array_[size_ - 1];
+	}
+
+	[[nodiscard]] size_t          empty()  const { return size_ == 0; }
 
 	[[nodiscard]] size_t          size()   const { return size_; }
 
-	[[nodiscard]] ElemType*       begin()        { return array_; }
+	[[nodiscard]] ElemType*       begin()  const { return array_; }
 	[[nodiscard]] const ElemType* cbegin() const { return array_; }
 
-	[[nodiscard]] ElemType*       end()          { return array_ + size_; }
+	[[nodiscard]] ElemType*       end()    const { return array_ + size_; }
 	[[nodiscard]] const ElemType* cend()   const { return array_ + size_; }
 };
 
