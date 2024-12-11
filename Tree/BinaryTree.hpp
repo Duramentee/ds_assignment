e739e476608c5de3b0a4ef693a91eecc56bb1713
drowlia <lsx3193161830@163.com>
2024-12-11 21:50:37 +0800

    写了二叉树,完成了插入,深搜,广搜,真难写.......,明天补上其他函数

--- Tree/BinaryTree.hpp
+++ Tree/BinaryTree.hpp
@@ -4,16 +4,117 @@
 
 #pragma once
 
+#include <iostream>
+
+#include "../Queue/Queue.hpp"
+
 namespace ds {
 
 template <typename ElemType>
-struct BinaryTree {
+struct BinaryTreeNode {
+	ElemType data_;
+	BinaryTreeNode* left_;
+	BinaryTreeNode* right_;
 
+	explicit BinaryTreeNode(const ElemType& value) : data_{value}, left_{nullptr}, right_{nullptr} {}
 };
 
+template <typename ElemType>
+struct BinaryTree {
+private:
+	BinaryTreeNode<ElemType>* root_;
 
+	void insert_util(BinaryTreeNode<ElemType>*& node, const ElemType& value) {
+		if (node == nullptr) {
+			node = new BinaryTreeNode<ElemType>(value);
+		} else if (value < node->data_) {
+			insert_util(node->left_, value);
+		} else {
+			insert_util(node->right_, value);
+		}
+	}
+	void visit(BinaryTreeNode<ElemType>* node) const {
+		std::cout << node->data_ << " ";
+	}
+	void pre_order_util(BinaryTreeNode<ElemType>* node) const {
+		if (node != nullptr) {
+			visit(node);
+			pre_order_util(node->left_);
+			pre_order_util(node->right_);
+		}
+	}
+	void in_order_util(BinaryTreeNode<ElemType>* node) const {
+		if (node != nullptr) {
+			in_order_util(node->left_);
+			visit(node);
+			in_order_util(node->right_);
+		}
+	}
+	void post_order_util(BinaryTreeNode<ElemType>* node) const {
+		if (node != nullptr) {
+			post_order_util(node->left_);
+			post_order_util(node->right_);
+			visit(node);
+		}
+	}
+	void destroy_tree(BinaryTreeNode<ElemType>* node) {
+		if (node != nullptr) {
+			destroy_tree(node->left_);
+			destroy_tree(node->right_);
+			delete node;
+		}
+	}
 
+public:
+	BinaryTree() : root_{nullptr} {}
 
+	~BinaryTree() {
+		destroy_tree(root_);
+	}
+
+	void insert(const ElemType& value) {
+		insert_util(root_, value);
+	}
+
+	void pre_order() const {
+		pre_order_util(root_);
+		std::cout << std::endl;
+	}
+
+	void in_order() const {
+		in_order_util(root_);
+		std::cout << std::endl;
+	}
+
+	void post_order() const {
+		post_order_util(root_);
+		std::cout << std::endl;
+	}
+
+	void level_order() const {
+		if (root_ == nullptr)
+			return;
+
+		ds::Queue<BinaryTreeNode<ElemType>*> q;
+		q.enqueue(root_);
+
+		while (!q.is_empty()) {
+			BinaryTreeNode<ElemType>* current = q.peek();
+			q.dequeue();
+
+			std::cout << current->data_ << " ";
+
+			if (current->left_ != nullptr) {
+				q.enqueue(current->left_);
+			}
+
+			if (current->right_ != nullptr) {
+				q.enqueue(current->right_);
+			}
+		}
+		std::cout << std::endl;
+	}
+};
 
 
 
--- Tree/BinaryTreeTest.cpp
+++ Tree/BinaryTreeTest.cpp
@@ -1,3 +1,88 @@
-//
-// Created by lsx31 on 24-12-9.
-//
+#include <gtest/gtest.h>
+#include <sstream>
+#include "BinaryTree.hpp"
+
+using namespace ds;
+
+// Helper function to capture standard output
+std::string captureStdout(std::function<void()> func) {
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+    func();
+    std::cout.rdbuf(old);
+    return buffer.str();
+}
+
+TEST(BinaryTreeTest, Insert) {
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    // The tree structure should be:
+    //         10
+    //        /  \
+    //       5    15
+    //      / \   / \
+    //     3   7 12  18
+}
+
+TEST(BinaryTreeTest, PreOrderTraversal) {
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    std::string output = captureStdout([&tree]() { tree.pre_order(); });
+    EXPECT_EQ(output, "10 5 3 7 15 12 18 \n");
+}
+
+TEST(BinaryTreeTest, InOrderTraversal) {
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    std::string output = captureStdout([&tree]() { tree.in_order(); });
+    EXPECT_EQ(output, "3 5 7 10 12 15 18 \n");
+}
+
+TEST(BinaryTreeTest, PostOrderTraversal) {
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    std::string output = captureStdout([&tree]() { tree.post_order(); });
+    EXPECT_EQ(output, "3 7 5 12 18 15 10 \n");
+}
+
+TEST(BinaryTreeTest, LevelOrderTraversal) {
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    std::string output = captureStdout([&tree]() { tree.level_order(); });
+    EXPECT_EQ(output, "10 5 15 3 7 12 18 \n");
+}
